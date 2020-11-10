43714a10b53c900e83132686adaa18866e1e9435
oijbaker <56763792+oijbaker@users.noreply.github.com>
2020-11-10 15:52:36 +0000

    Add files via upload

--- CardGame.java
+++ CardGame.java
@@ -3,19 +3,18 @@ import java.util.Scanner;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.File;
-//import org.junit.Test;
+import java.util.Arrays;
+
 
 public class CardGame {
 
     public static Player[] players;
     public static Deck[] decks;
 
-	public CardGame() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	//@Test
-	//public void testCardGame() throws IOException {
+    public CardGame() {
+        // TODO Auto-generated constructor stub
+    }
+    
     public static void main(String[] args) {
 
         // Ask the user for the number of players
@@ -80,18 +79,7 @@ public class CardGame {
             }
         }
 
-        // print out all the players' hands
-        for (Player player : players) {
-            System.out.print(player.getPlayerNumber() + ": ");
-            player.printHand();
-        }
-
-        // print out all the decks
-        for (Deck deck : decks) {
-            System.out.print(deck.getDeckNumber() + ": ");
-            deck.printDeck();
-        }
-
+        // create the player threads
         Thread[] threads = new Thread[numPlayers];
         for (int i = 0; i < numPlayers; i++) {
             Thread t = new Thread(players[i]);
@@ -99,16 +87,35 @@ public class CardGame {
             t.start();
         }
 
+        // while there is no winner...
         boolean winnerFound = false;
         while (!winnerFound) {
+            // check whether each player has won.
             for (Player p : players) {
                 if (p.checkWin()) {
-                    System.out.println("Player " + p.getPlayerNumber() + " wins.");
-                    for (Player p1 : players) {
-                        p1.setWinnerFound(true);
+                    // if the have, write this to the file, set the winnerFound flag to true...
+                    String fileLocation = new File("").getAbsolutePath();
+                    try {
+                        p.getWriter().write("Player " + p.getPlayerNumber() + " wins.");
+                        winnerFound = true;
+                        // for each player, write that the player has been informed to stop
+                        for (Player p1 : players) {
+                            p1.setWinnerFound(true);
+                            if (p1 != p) {
+                                p1.getWriter().write("Player " + p.getPlayerNumber() + " has informed player "
+                                        + p1.getPlayerNumber() + " that player " + p.getPlayerNumber() + " has won." + "\n");
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    // interrupt the threads
+                    for (Thread t : threads) {
+                        t.interrupt();
                     }
                     break;
                 } else {
+                    // otherwise stop the players from waiting
                     for (Player p1 : players) {
                         synchronized (p1) {
                             p1.notify();
@@ -119,6 +126,7 @@ public class CardGame {
         }
 
 
+        // join the threads
         for (Thread t : threads) {
             try {
                 t.join();
@@ -127,36 +135,29 @@ public class CardGame {
             }
         }
 
-        // Play the game
-        /*
-        Boolean winnerFound = false;
-        Player winner = null;
-        
-        // while nobody has won
-        while (winnerFound == false) {
-            // each player check fir a win, and draws a card if not
-            for (Player player : players) {
-                decks[player.getPlayerNumber() - 1].printDeck();
-                if (player.checkWin()) {
-                    winnerFound = true;
-                    winner = player;
-                    try {
-                        player.getWriter().write("Winner is: " + String.valueOf(player.getPlayerNumber() + "\n"));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    break;
-                } else {
-                    player.drawCard();
-                }
-            }
-        }*/
-
+        // write the players' final hands to the files
         for (Player player : players) {
             try {
                 FileWriter w = player.getWriter();
                 w.write(player.getPlayerNumber() + " Final Hand: " + player.toString() + "\n");
                 w.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // create deck files and write the final state
+        String fileLocation = new File("").getAbsolutePath();
+        for (Deck d : decks) {
+            try {
+                String filename = fileLocation + "\\CardGameFiles\\deck" + d.getDeckNumber() + "_ouput.txt";
+                FileWriter deckWriter = new FileWriter(filename);
+                String deckString = "";
+                for (Card c : d.getDeck()) {
+                    deckString += c.readValue() + " ";
+                }
+                deckWriter.write("Deck " + d.getDeckNumber() + " Final Hand: " + deckString);
+                deckWriter.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
